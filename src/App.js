e2c1b5e66c34361a3b3fbdf7a672d4b126ba4a54
Nabeel Akbar <studiosnabeel@gmail.com>
2023-01-17 16:58:24 +0500

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Details from './components/Details';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <>
+      <Header />
+      <Home />
+      <Details />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Jeans from '../Assets/Jeans.jpg';
+
+const Home = () => (
+  <div className="flex m-4 justify-center flex-wrap">
+    <div className="bg-red-200 p-2 m-4">
+      <img src={Jeans} alt="jeans" className="w-40" />
+      <h2 className="px-2">Mens Jeans</h2>
+      <p>
+        <span className="px-2">Price</span>
+        <span>£20.00</span>
+      </p>
+    </div>
+    <div className="bg-red-200 p-2 m-4">
+      <img src={Jeans} alt="jeans" className="w-40" />
+      <h2 className="px-2">Mens Jeans</h2>
+      <p>
+        <span className="px-2">Price</span>
+        <span>£20.00</span>
+      </p>
+    </div>
+    <div className="bg-red-200 p-2 m-4">
+      <img src={Jeans} alt="jeans" className="w-40" />
+      <h2 className="px-2">Mens Jeans</h2>
+      <p>
+        <span className="px-2">Price</span>
+        <span>£20.00</span>
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
