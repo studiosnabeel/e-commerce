5150ce8049042b251eb029fa36b95a996dcbde77
Nabeel Akbar <studiosnabeel@gmail.com>
2023-01-17 19:45:16 +0500

    Add Routes and components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
 import Home from './components/Home';
-import Details from './components/Details';
+import About from './components/About';
+import Products from './components/Products';
+import Contact from './components/Contact';
+import Singleproduct from './components/Singleproduct';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
-      <Details />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/singleproduct/:id" element={<Singleproduct />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/components/Singleproduct.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Singleproduct = () => (
+  <div>Singleproduct</div>
+);
+
+export default Singleproduct;
